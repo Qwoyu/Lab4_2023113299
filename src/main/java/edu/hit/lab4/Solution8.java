443a5cf60f2088c1aa53b6cda049ddf3fdc7db19
Qwoyu <1958632871@qq.com>
2025-12-01 19:55:54 +0800

    Init Lab4 Maven project with CI

---
+++ src/main/java/edu/hit/lab4/Solution8.java
@@ -0,0 +1,55 @@
+package edu.hit.lab4;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 计算二维网格中的岛屿数量：'1' 为陆地，'0' 为水。
+ */
+public class Solution8 {
+    void bfs(char[][] grid, int sr, int sc) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+        Deque<int[]> q = new ArrayDeque<>();
+        grid[sr][sc] = '0';
+        q.offer(new int[]{sr, sc});
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0], c = cur[1];
+            if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+                grid[r - 1][c] = '0';
+                q.offer(new int[]{r - 1, c});
+            }
+            if (r + 1 < nr && grid[r + 1][c] == '1') {
+                grid[r + 1][c] = '0';
+                q.offer(new int[]{r + 1, c});
+            }
+            if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+                grid[r][c - 1] = '0';
+                q.offer(new int[]{r, c - 1});
+            }
+            if (c + 1 < nc && grid[r][c + 1] == '1') {
+                grid[r][c + 1] = '0';
+                q.offer(new int[]{r, c + 1});
+            }
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int numIslands = 0;
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+                    numIslands++;
+                    bfs(grid, r, c);
+                }
+            }
+        }
+        return numIslands;
+    }
+}
---
+++ src/test/java/edu/hit/lab4/Solution8Test.java
@@ -0,0 +1,46 @@
+package edu.hit.lab4;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Solution8Test {
+    @Test
+    public void example1() {
+        char[][] grid = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
+        };
+        Solution8 s = new Solution8();
+        assertEquals(1, s.numIslands(copy(grid)));
+    }
+
+    @Test
+    public void example2() {
+        char[][] grid = {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+        Solution8 s = new Solution8();
+        assertEquals(3, s.numIslands(copy(grid)));
+    }
+
+    @Test
+    public void emptyGrid() {
+        char[][] grid = {};
+        Solution8 s = new Solution8();
+        assertEquals(0, s.numIslands(grid));
+    }
+
+    private char[][] copy(char[][] g) {
+      if (g.length == 0) return g;
+      char[][] c = new char[g.length][g[0].length];
+      for (int i=0;i<g.length;i++) {
+        System.arraycopy(g[i],0,c[i],0,g[i].length);
+      }
+      return c;
+    }
+}
